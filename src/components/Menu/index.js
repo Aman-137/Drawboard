ecae8dad1be993a528fb82979954e2be086072ed
aman <kumaramansingh137@gmail.com>
2025-08-17 10:38:27 +0530

    add ability to download

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -11,7 +11,7 @@ import styles from "./index.module.css";
 import cx from "classnames";
 import { useDispatch, useSelector } from "react-redux";
 import { MENU_ITEMS } from "@/constants";
-import { menuItemClick } from "@/slice/menuSlice";
+import { actionItemClick, menuItemClick } from "@/slice/menuSlice";
 
 const Menu = () => {
   const dispatch = useDispatch();
@@ -19,6 +19,10 @@ const Menu = () => {
 
   const handleMenuClick = (itemName) => {
     dispatch(menuItemClick(itemName));
+  };
+
+  const handleActionItemClick = (itemName) => {
+    dispatch(actionItemClick(itemName));
   };
   return (
     <div className={styles.menuContainer}>
@@ -38,13 +42,22 @@ const Menu = () => {
       >
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}
+      >
         <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}
+      >
         <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}
+      >
         <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
       </div>
     </div>
