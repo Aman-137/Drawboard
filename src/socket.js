43ecee2b2959db9a32df70f945f5a7033f605cbc
aman <kumaramansingh137@gmail.com>
2025-08-21 01:25:40 +0530

    Provide BE endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,7 @@
 const { io } = require("socket.io-client");
 
-export const socket = io("http://localhost:5000");
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://drawboard-server.onrender.com"
+    : "http://localhost:5000";
+export const socket = io(URL);
